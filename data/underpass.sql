378a6c4ee659704b408bc71b2f741231a181796b
Rob Savoye <rob@senecass.com>
2020-11-26 11:19:21 -0700

    Scheme for Underpass

---
+++ data/underpass.sql
@@ -0,0 +1,39 @@
+CREATE EXTENSION IF NOT EXISTS hstore WITH SCHEMA public;
+CREATE EXTENSION IF NOT EXISTS postgis WITH SCHEMA public;
+
+CREATE TYPE interval AS ENUM('minute', 'hour', 'day', 'changeset');
+
+CREATE TABLE public.states (
+    timestamp timestamp without time zone DEFAULT now() NOT NULL,
+    path text NOT NULL,
+    sequence integer NOT NULL,
+    frequency text NOT NULL
+);
+
+CREATE TABLE public.way_nodes (
+    way_id bigint NOT NULL,
+    node_id bigint NOT NULL,
+    sequence_id integer NOT NULL
+);
+
+CREATE TABLE public.ways (
+    id bigint NOT NULL,
+    version integer NOT NULL,
+    user_id integer NOT NULL,
+    tstamp timestamp without time zone NOT NULL,
+    changeset_id bigint NOT NULL,
+    tags public.hstore,
+    nodes bigint[],
+    bbox public.geometry(Geometry,4326),
+    linestring public.geometry(Geometry,4326)
+);
+
+CREATE TABLE public.nodes (
+    id bigint NOT NULL,
+    version integer NOT NULL,
+    user_id integer NOT NULL,
+    tstamp timestamp without time zone NOT NULL,
+    changeset_id bigint NOT NULL,
+    tags public.hstore,
+    geom public.geometry(Point,4326)
+);
