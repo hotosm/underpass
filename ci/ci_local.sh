ade9e3f19a5a7eee97fc3f8deab6a1ab0c9b072d
Alessandro Pasotti <elpaso@itopen.it>
2021-08-05 12:38:51 +0200

    Add ci_local.sh utility

---
+++ ci/ci_local.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Runs the CI workflow locally using docker-compose, the issued commands
+# MUST be kept in sync with those in .github/workflows/run_tests.yml
+
+
+TEMP_DIR=`mktemp -d -t underpass_ci_local_XXXX`
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+echo "Running Underpass CI tests in temporary directory: ${TEMP_DIR}"
+
+
+cp -r ${SCRIPT_DIR}/.. ${TEMP_DIR}
+pushd ${PWD}
+
+cd ${TEMP_DIR}
+
+# Just in case
+make distclean -j `nproc`|| true
+
+DOCKER_DIR="${TEMP_DIR}/docker"
+DOCKER_COMPOSE_FILE="${DOCKER_DIR}/docker-compose.yml"
+DOCKER_BASE_COMMAND="docker-compose -f ${DOCKER_COMPOSE_FILE}"
+
+# Run the composition
+${DOCKER_BASE_COMMAND} up -d
+
+# Prepare Test Database
+${DOCKER_BASE_COMMAND} exec -T postgis psql -U underpass_test -f /code/ci/create_underpass_db.sql
+
+# Build Underpass Library and Binaries
+${DOCKER_BASE_COMMAND} exec -T underpass-build-deps sh -c "cd /code && ./autogen.sh && (rm -rf build || true) && mkdir build && cd build && ../configure && make -j `nproc`"
+
+# Build and Run Underpass Tests
+${DOCKER_BASE_COMMAND} exec -T underpass-build-deps sh -c "cd /code/build && make check -j `nproc`"
+
+# Comment the cleanup lines below or exit here if you want to run additional
+# tests from a console in the temp container, for instance with:
+# ${DOCKER_BASE_COMMAND} exec underpass-build-deps bash
+${DOCKER_BASE_COMMAND} down
+echo "Remove temporary folder ${TEMP_DIR}"
+sudo rm -rf ${TEMP_DIR}
+
+popd
