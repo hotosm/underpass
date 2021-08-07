98f88035d43ba5775287ccf21a8292815b945807
Rob Savoye <rob@senecass.com>
2021-08-07 10:14:17 -0600

    Only truncate the log file if it exists

--- log.cc
+++ log.cc
@@ -450,7 +450,9 @@ LogFile::openLog(const std::string& filespec)
         _state = CLOSED;
     }
 
-    boost::filesystem::resize_file(filespec, 0);
+    if (boost::filesystem::exists(filespec)){
+        boost::filesystem::resize_file(filespec, 0);
+    }
     // FIXME: Append, don't truncate, the log file
     _outstream.open(filespec, std::ios::app|std::ios::out); // ios::out
     if( _outstream.fail() ) {
