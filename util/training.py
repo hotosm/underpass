c5debf4f2820c2d174f323ddeb2da2cc1b3671a2
Rob Savoye <rob@senecass.com>
2021-11-27 09:32:30 -0700

    Add enums to match the DB schema, figure out upsert

--- util/training.py
+++ util/training.py
@@ -19,25 +19,100 @@
 #
 
 import argparse
-import psycopg2
 import sys
 import os
 import epdb
 import logging
+import enum
 import pandas as pd
-import pathlib
-import csv
+from sqlalchemy import create_engine, text
+from sqlalchemy import Table, Column, MetaData, Integer, String, ARRAY, Boolean, DateTime
+from sqlalchemy import Enum, BigInteger
+from sqlalchemy.dialects.postgresql import insert
 
+
+#
+# These enums match what the database schema also supports
+#
+class GenderEnum(enum.Enum):
+    male = 'male'
+    female = 'female'
+    nonbinary =  'nonbinary'
+    other = 'other'
+    private = 'private'
+
+class AgeEnum(enum.Enum):
+    child = '10-14'
+    teen = '15-24'
+    adult = '25-40'
+    mature = '41+'
+
+    
+class DesignationEnum(enum.Enum):
+     volunteer = 'volunteer'
+     intern = 'intern'
+     civil = 'civil'
+     gis = 'gis'
+     director = 'director'
 
 class Training(object):
     def __init__(self, db=None, host=None):
-        """ """
+        """Parse training data"""
+        self.data = dict()
+        conn = "postgresql+psycopg2://"
+        if host is not None:
+            conn += host + "/"
+        else:
+            conn += "localhost/"
+        if db is None:
+            conn += "galaxy"
+        else:
+            conn += db
+        self.engine = create_engine(conn)
+        usermeta = MetaData()
+        self.users = Table(
+            "users",
+            usermeta,
+            Column('id', BigInteger),
+            Column('name', String),
+            Column('gender', Enum(GenderEnum)),
+            Column('agerange', Enum(AgeEnum)),
+            Column('topics', ARRAY(String)),
+            Column('country', String),
+            Column('designation', Enum(DesignationEnum)),
+            Column('organization', Integer),
+            Column('youthmapper', Boolean),
+            )
+        trainmeta = MetaData()
+        self.training = Table(
+            "training",
+            trainmeta,
+            Column('name', String),
+            Column('topics', ARRAY(String)),
+            Column('location', String),
+            Column('type', Boolean),
+            Column('organization', Integer),
+            Column('hours', Integer),
+            Column('timestamp', DateTime),
+            )
+        with self.engine.connect() as conn:
+            sql = insert(self.users).values(name='foobar', id=123456)
+            sql = sql.on_conflict_do_update(
+                index_elements=['id'],
+                set_=dict(id=123456, name='barfoo', gender='nonbinary')
+            )
+            print(sql)
+            result = conn.execute(sql)
+        #     print(result.all())
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='import training spreadsheet into Galaxy database')
     parser.add_argument("--infile","-i", help='The input spreadsheet')
+    parser.add_argument("--dburl","-d", help='The output database URL')
     parser.add_argument("--verbose","-v", default='yes', help='Enable verbosity')
     args = parser.parse_args()
+
+    training = Training()
 
     # if verbose, dump to the terminal as well as the logfile.
     if args.verbose == "yes":
@@ -60,47 +135,27 @@ if __name__ == '__main__':
         for name,foo in df_sheet_all.items():
             #print(name)
             sheet = pd.read_excel(args.infile, sheet_name='20210423')
-            for col in df.columns:
-                print(col)
-            df.dropna(how="any")
-            head1 = df.iloc[0]
-            df = df[1:]
-            head2 = df.iloc[6]
-            #print(head2)
-            df.columns = head2
-            #print(df)
-            #print(sheet)
-            break
     else:
         df = pd.read_csv(args.infile)
-        df.dropna(how="any")
-        head1 = df.iloc[0]
-        print(head1)
-        # df = df[1:]
-        # head2 = df.iloc[7]
-        # print("========================")
-        # print(head2)
-        # df.columns = head2
-        #print(df)
-        for col in df.columns:
-            print(col)
-
-        for index, row in df.iterrows():
-            data = dict()
-            #print(index)
-            #print('~~~~~~')
-            #print(row)
-            if type(row[0]) == str:
-                if row[0].isdigit():
-                    i = 0
-                    while i < head2.size:
-                        print("FIXME: %r" % row[i])
-                        if type(head2[i]) == float:
-                            head2[i] = "id"
-                        data[head2[i]] = row[i]
-                        i = i + 1
-                    print(data)
-                else:
-                    head2 = df.iloc[index + 1]
-                    df.columns = head2
-            print('------')        # # Drop rows which contain any NaN values
+        
+    df.dropna(how="any")
+    head1 = df.iloc[0]
+    print(head1)
+    for col in df.columns:
+        print(col)
+        
+    for index, row in df.iterrows():
+        data = dict()
+        if type(row[0]) == str:
+            if row[0].isdigit():
+                i = 0
+                while i < head2.size:
+                    # print("FIXME: %r" % row[i])
+                    if type(head2[i]) == float:
+                        head2[i] = "id"
+                    data[head2[i]] = row[i]
+                    i = i + 1
+                print(data)
+            else:
+                head2 = df.iloc[index + 1]
+                df.columns = head2
