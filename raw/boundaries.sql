9b58971f884e13fbeda64eb68fee13f13b7b51dd
itskshitiz321 <skshitizraj@gmail.com>
2022-02-21 17:12:03 +0545

    added license

--- raw/boundaries.sql
+++ raw/boundaries.sql
@@ -1,3 +1,21 @@
+--
+-- Copyright (c) 2020, 2021 Humanitarian OpenStreetMap Team
+--
+-- This file is part of Underpass.
+--
+--     Underpass is free software: you can redistribute it and/or modify
+--     it under the terms of the GNU General Public License as published by
+--     the Free Software Foundation, either version 3 of the License, or
+--     (at your option) any later version.
+--
+--     Underpass is distributed in the hope that it will be useful,
+--     but WITHOUT ANY WARRANTY; without even the implied warranty of
+--     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--     GNU General Public License for more details.
+--
+--     You should have received a copy of the GNU General Public License
+--     along with Underpass.  If not, see <https:--www.gnu.org/licenses/>.
+
 CREATE TABLE IF NOT EXISTS boundaries (
 	name_en varchar NULL,
 	admin_level int4 NULL,
--- raw/raw.lua
+++ raw/raw.lua
@@ -1,3 +1,20 @@
+-- Copyright (c) 2020, 2021 Humanitarian OpenStreetMap Team
+--
+-- This file is part of Underpass.
+--
+--     Underpass is free software: you can redistribute it and/or modify
+--     it under the terms of the GNU General Public License as published by
+--     the Free Software Foundation, either version 3 of the License, or
+--     (at your option) any later version.
+--
+--     Underpass is distributed in the hope that it will be useful,
+--     but WITHOUT ANY WARRANTY; without even the implied warranty of
+--     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--     GNU General Public License for more details.
+--
+--     You should have received a copy of the GNU General Public License
+--     along with Underpass.  If not, see <https:--www.gnu.org/licenses/>.
+
 -- This is lua script for osm2pgsql in order to create and process custom schema to store incoming osm data efficiently
 
 -- osm2pgsql --create -H localhost -U admin -P 5432 -d postgres -W --extra-attributes --output=flex --style ./raw.lua nepal-latest-internal.osm.pbf 
