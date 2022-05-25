cbd16b6552a695d3f3f1c348dec074274ec3a6da
eternaltyro <eternaltyro@protonmail.ch>
2022-05-25 17:29:47 +0530

    Fix zone_id value for Route53 entry

--- infra/infra.tf
+++ infra/infra.tf
@@ -360,7 +360,7 @@ resource "aws_route53_record" "galaxy-api-lb" {
 
   alias {
     name                   = aws_lb.galaxy-api.dns_name
-    zone_id                = aws_lb.galaxy-api.dns_name
+    zone_id                = aws_lb.galaxy-api.zone_id
     evaluate_target_health = true
   }
 }
