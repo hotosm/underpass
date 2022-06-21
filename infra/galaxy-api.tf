f01e89624e1f83a68247cd889a6f9f29a9a70255
eternaltyro <eternaltyro@protonmail.ch>
2022-06-21 15:13:07 +0530

    Redirect HTTP to HTTPS for Galaxy API

--- infra/galaxy-api.tf
+++ infra/galaxy-api.tf
@@ -373,6 +373,22 @@ resource "aws_lb_listener" "galaxy-api-secure" {
   }
 }
 
+resource "aws_lb_listener" "galaxy-api-insecure" {
+  load_balancer_arn = aws_lb.galaxy-api.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
 data "aws_acm_certificate" "wildcard" {
   domain      = "hotosm.org"
   statuses    = ["ISSUED"]
