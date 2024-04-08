62acc6e7dc48894682c1f5a11091ca0c1c8a5252
Emillio Mariscal <emi420@gmail.com>
2024-04-08 18:28:27 -0300

    Save timestamp when a relation geometry was modified

--- src/raw/queryraw.cc
+++ src/raw/queryraw.cc
@@ -403,13 +403,17 @@ QueryRaw::applyChange(const OsmRelation &relation) const
                 // Update geometry only
 
                 query = "UPDATE relations SET ";
-                std::string format = "geom=%s WHERE osm_id=%d;";
+                std::string format = "geom=%s, timestamp=%s WHERE osm_id=%d;";
                 boost::format fmt(format);
 
                 // Geometry
                 std::string geometry;
                 geometry = "ST_GeomFromText(\'" + geostring + "\', 4326)";
                 fmt % geometry;
+
+                // Timestamp
+                std::string timestamp = to_simple_string(boost::posix_time::microsec_clock::universal_time());
+                fmt % timestamp;
 
                 // osm_id
                 fmt % relation.id;
