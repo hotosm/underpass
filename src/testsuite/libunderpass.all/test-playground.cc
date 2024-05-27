705a7ade71ed58cc7930f6fe75a57a051a24bf5e
Rob Savoye <rob@senecass.com>
2024-05-27 10:35:15 -0600

    fix: Add some tests for testing new code

--- src/testsuite/libunderpass.all/test-playground.cc
+++ src/testsuite/libunderpass.all/test-playground.cc
@@ -18,9 +18,69 @@
 //
 
 #include <iostream>
+#include "raw/queryraw.hh"
+#include "osm/osmobjects.hh"
+#include <boost/date_time.hpp>
+
+using namespace queryraw;
+using namespace osmobjects;
+
+logger::LogFile &dbglogfile = logger::LogFile::getDefaultInstance();
 
 int main() {
+
+    dbglogfile.setVerbosity();
+
+    auto pq = std::make_shared<Pq>();
+    pq->connect("colorado");
+    auto sqlprep = std::make_shared<SQLPrep>(pq);
+    std::vector<osmobjects::OsmNode> nodes;
+
+    OsmNode node1;
+    node1.id = 11111;
+    ptime now  = boost::posix_time::second_clock::universal_time();
+
+    // node1.timestamp =
+    node1.changeset = 22222;
+    node1.timestamp = now;
+    node1.addTag("place", "city");
+    node1.addTag("name", "Nederland");
+    node1.user = "testuser";
+    boost::geometry::read_wkt("POINT (30 10)", node1.point);
+    nodes.push_back(node1);
+
+    OsmNode node2;
+    node2.id = 3333;
+    node2.changeset = 4444;
+    node1.timestamp = now;
+    node2.addTag("place", "city");
+    node2.addTag("name", "Rollinsville");
+    node2.user = "test user";
+    boost::geometry::read_wkt("POINT (40 20)", node2.point);
+    nodes.push_back(node2);
+
+    if (sqlprep->insert_entries(nodes)) {
+        std::cerr << "Inserted a node" << std::endl;
+    } else {
+        std::cerr << "Couldn't inserted a node" << std::endl;
+    }
+
+#if 0
+    auto delnode = InsertObject(node);
+    delnode.dump();
+    delnode.buildQuery("nodes");
+    OsmWay way;
+    std::vector<int> refs = {1, 22, 333, 4444};
+    way.id = 33333;
+    way.changeset = 44444;
+    way.addTag("building", "yes");
+
+    auto delway = InsertObject(way);
+    delway.dump();
+    delway.buildQuery("way_poly");
+
     return 0;
+#endif
 }
 
 // local Variables:
